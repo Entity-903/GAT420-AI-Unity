c163ccb4035c85cf8848682097a200019dd92118
Entity-903 <charlesmassie021@gmail.com>
2024-01-11 01:46:58 -0700

    Missing code from 1/11

--- Assets/Scripts/AIAutonomousAgent.cs
+++ Assets/Scripts/AIAutonomousAgent.cs
@@ -2,12 +2,16 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class AutonomousAgent : MonoBehaviour
+public class AutonomousAgent : AIAgent
 {
     public AIPerception perception = null;
 
     private void Update()
     {
-        //
+        var gameObjects = perception.GetGameObjects();
+        foreach (var go in gameObjects)
+        {
+            Debug.DrawLine(transform.position, go.transform.position);
+        }
     }
 }
